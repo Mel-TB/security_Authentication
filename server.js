9b46bddd1e78aea751f8d24a4e948b6e0664ce59
Mel-TB <melinda.trami@live.fr>
2023-10-14 16:03:16 +0200

    Feature: Add Google Oauth - Implement Authentication with Google and OAuth using passeport.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const path = require("path");
 const https = require("https");
 const helmet = require("helmet");
 const express = require("express");
+const passport = require("passport");
+const googleStrategy = require("passport-google-oauth20");
 
 require("dotenv").config();
 
@@ -13,9 +15,23 @@ const config = {
   CLIENT_SECRET: process.env.CLIENT_SECRET,
 };
 
+const AUTH_OPTIONS = {
+  callbackURL: "/auth/google/callback",
+  clientID: config.CLIENT_ID,
+  clientSecret: config.CLIENT_SECRET,
+};
+
+function verifyCallBack(accessToken, refreshToken, profile, done) {
+  console.log("Google Profile:", profile);
+  done(null, profile);
+}
+
+passport.use(new googleStrategy(AUTH_OPTIONS, verifyCallBack));
+
 const app = express();
 
 app.use(helmet());
+app.use(passport.initialize());
 
 function checkLoggedIn(req, res, next) {
   const isLoggedIn = true; // todo:
@@ -27,14 +43,33 @@ function checkLoggedIn(req, res, next) {
   next();
 }
 
-app.get("/auth/google", (req, res) => {});
+app.get(
+  "/auth/google",
+  passport.authenticate("google", {
+    scope: ["email"],
+  })
+);
 
-app.get("/auth/google/callback", (req, res) => {});
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    failureRedirect: "/failure",
+    successRedirect: "/",
+    session: false,
+  }),
+  (req, res) => {
+    console.log("Google called us back");
+  }
+);
 
 app.get("/auth/logout", (req, res) => {});
 
 app.get("/secret", checkLoggedIn, (req, res) => {
   return res.send("Your secret is 1234");
+});
+
+app.get("/failure", (req, res) => {
+  return res.send("Failed to authenticate try again");
 });
 
 app.get("/", (req, res) => {
