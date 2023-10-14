89491a2bba2c32a96ae9582b36d5957126950611
Mel-TB <melinda.trami@live.fr>
2023-10-14 15:33:34 +0200

    Features: Start OAuth with google: - Start configure for OAuth authentication

---
+++ server.js
@@ -0,0 +1,54 @@
+const fs = require("fs");
+const path = require("path");
+const https = require("https");
+const helmet = require("helmet");
+const express = require("express");
+
+require("dotenv").config();
+
+const PORT = 3000;
+
+const config = {
+  CLIENT_ID: process.env.CLIENT_ID,
+  CLIENT_SECRET: process.env.CLIENT_SECRET,
+};
+
+const app = express();
+
+app.use(helmet());
+
+function checkLoggedIn(req, res, next) {
+  const isLoggedIn = true; // todo:
+  if (!isLoggedIn) {
+    return res.status(401).json({
+      error: "You must be logged in to access this resource",
+    });
+  }
+  next();
+}
+
+app.get("/auth/google", (req, res) => {});
+
+app.get("/auth/google/callback", (req, res) => {});
+
+app.get("/auth/logout", (req, res) => {});
+
+app.get("/secret", checkLoggedIn, (req, res) => {
+  return res.send("Your secret is 1234");
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem"),
+    },
+    app
+  )
+  .listen(PORT, () => {
+    console.log(`Listening on port ${PORT}`);
+  });
